c413f497980f178af5d220de92a120f4743cd76b
Prospero <109393624+ProsperoAngelo@users.noreply.github.com>
2024-09-16 22:35:38 -0300

    Adicionados comentários no código
    Foram adicionados comentarios e removidos comandos de saídas no console para controle.

--- dados-atletas.js
+++ dados-atletas.js
@@ -6,7 +6,8 @@ class Atletas {
         this.altura = altura
         this.notas = notas
     }
-
+    
+//Os metodos retornam os atriutos dos atletas
     obtemNomeAtleta(){
         return this.nome
     }
@@ -35,6 +36,7 @@ class Atletas {
         return this.calculaMediaValida().toFixed(8)
     }
 
+//A condicional determina a categoria do atleta com baase em sua idade
     calculaCategoria(){
         if (this.idade >= 9 && this.idade <= 11){
             return "Infantil"
@@ -48,25 +50,32 @@ class Atletas {
             return "Sem categoria"
         }
     }
-
+//Calcula o IMC
     calculaIMC(){
         return this.peso / (this.altura * this.altura)
     }
 
     calculaMediaValida(){
+//Organiza as notas do menor para o maior
         this.notas.sort(function(a,b){
             return a-b  
         })
+
+//Elimina a maior e a menor nota 
         let notasValidas = this.notas.slice(1,4)
+
+//Reduz o vaalor das 3 notas validas em um único valor e o guarda na variável soma para futuro cálculo de média
         let soma = notasValidas.reduce(function(atual, total){
             return total + atual
         },0)
-        console.log(soma)
+
+//Calcula a média das notas dos atletas
         let media = soma/notasValidas.length
         return media
     }
 }
 
+//Classe instanciada com dados de um determinado atleta
 const atleta = new Atletas(
     "Cesar Abascal", 
     30,
@@ -74,6 +83,7 @@ const atleta = new Atletas(
     1.7,
     [10,9.34,8.42,10,7.88])
 
+//Exibe no console os dados dos atletas
 console.log(`Nome: ${atleta.nome}`)
 console.log(`Idade: ${atleta.idade}`)
 console.log(`Peso: ${atleta.peso}`)
