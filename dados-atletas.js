4176bf0a37c04f662ddb2056928bf312f677dbc7
Prospero <109393624+ProsperoAngelo@users.noreply.github.com>
2024-09-17 22:37:30 -0300

    Corrigida saída de dados
    Foi modificada a saída no console: alguns dados estavam sendo apresentados diretamente da classe atleta, sem a invocação do método referente ao dado.

--- dados-atletas.js
+++ dados-atletas.js
@@ -1,94 +1,98 @@
 class Atletas {
-    constructor (nome, idade, peso, altura, notas){
-        this.nome = nome
-        this.idade = idade
-        this.peso = peso
-        this.altura = altura
-        this.notas = notas
-    }
-    
+  constructor (nome, idade, peso, altura, notas){
+      this.nome = nome
+      this.idade = idade
+      this.peso = peso
+      this.altura = altura
+      this.notas = notas
+  }
+
 //Os metodos retornam os atriutos dos atletas
-    obtemNomeAtleta(){
-        return this.nome
-    }
+  obtemNomeAtleta(){
+      return this.nome
+  }
 
-    obtemIdadeAtleta(){
-        return this.idade
-    }
+  obtemIdadeAtleta(){
+      return this.idade
+  }
 
-    obtemPesoAtleta(){
-        return this.peso
-    }
+  obtemPesoAtleta(){
+      return this.peso
+  }
 
-    obtemNotasAtleta(){
-        return this.notas
-    }
+  obtemAlturaAtleta(){
+      return this.altura
+  }
+    
+  obtemNotasAtleta(){
+      return this.notas
+  }
 
-    obtemCategoria(){
-        return this.calculaCategoria()
-    }
+  obtemCategoria(){
+      return this.calculaCategoria()
+  }
 
-    obtemIMC(){ 
-        return this.calculaIMC()
-    }
+  obtemIMC(){ 
+      return this.calculaIMC()
+  }
 
-    obtemMediaValida(){
-        return this.calculaMediaValida().toFixed(8)
-    }
+  obtemMediaValida(){
+      return this.calculaMediaValida().toFixed(8)
+  }
 
 //A condicional determina a categoria do atleta com baase em sua idade
-    calculaCategoria(){
-        if (this.idade >= 9 && this.idade <= 11){
-            return "Infantil"
-        }else if(this.idade == 12 || this.idade == 13){
-            return "Juvenil"
-        }else if(this.idade == 14 || this.idade == 15){
-            return "Intermediário"
-        }else if(this.idade >=16 && this.idade <= 30){
-            return "Adulto"
-        }else{
-            return "Sem categoria"
-        }
-    }
+  calculaCategoria(){
+      if (this.idade >= 9 && this.idade <= 11){
+          return "Infantil"
+      }else if(this.idade == 12 || this.idade == 13){
+          return "Juvenil"
+      }else if(this.idade == 14 || this.idade == 15){
+          return "Intermediário"
+      }else if(this.idade >=16 && this.idade <= 30){
+          return "Adulto"
+      }else{
+          return "Sem categoria"
+      }
+  }
 //Calcula o IMC
-    calculaIMC(){
-        return this.peso / (this.altura * this.altura)
-    }
+  calculaIMC(){
+      return this.peso / (this.altura * this.altura)
+  }
 
-    calculaMediaValida(){
+  calculaMediaValida(){
 //Organiza as notas do menor para o maior
-        this.notas.sort(function(a,b){
-            return a-b  
-        })
+      this.notas.sort(function(a,b){
+          return a-b  
+      })
 
 //Elimina a maior e a menor nota 
-        let notasValidas = this.notas.slice(1,4)
+      let notasValidas = this.notas.slice(1,4)
 
 //Reduz o vaalor das 3 notas validas em um único valor e o guarda na variável soma para futuro cálculo de média
-        let soma = notasValidas.reduce(function(atual, total){
-            return total + atual
-        },0)
+      let soma = notasValidas.reduce(function(atual, total){
+          return total + atual
+      },0)
 
 //Calcula a média das notas dos atletas
-        let media = soma/notasValidas.length
-        return media
-    }
+      let media = soma/notasValidas.length
+      return media
+  }
 }
 
 //Classe instanciada com dados de um determinado atleta
 const atleta = new Atletas(
-    "Cesar Abascal", 
-    30,
-    80,
-    1.7,
-    [10,9.34,8.42,10,7.88])
+  "Cesar Abascal", 
+  30,
+  80,
+  1.7,
+  [10,9.34,8.42,10,7.88])
 
 //Exibe no console os dados dos atletas
-console.log(`Nome: ${atleta.nome}`)
-console.log(`Idade: ${atleta.idade}`)
-console.log(`Peso: ${atleta.peso}`)
-console.log(`Altura: ${atleta.altura}`)
-console.log(`Notas: ${atleta.notas}`)
+console.log(`Nome: ${atleta.obtemNomeAtleta()}`)
+console.log(`Idade: ${atleta.obtemNomeAtleta()}`)
+console.log(`Peso: ${atleta.obtemPesoAtleta()}`)
+console.log(`Altura: ${atleta.obtemAlturaAtleta()}`)
+console.log(`Notas: ${atleta.obtemNotasAtleta()}`)
 console.log(`Categoria: ${atleta.obtemCategoria()}`)
 console.log(`IMC: ${atleta.obtemIMC()}`)
 console.log(`Média Válida: ${atleta.obtemMediaValida()}`)
